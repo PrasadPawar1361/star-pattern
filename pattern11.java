fa9fdc49a6efa8aa3ec9be7a50d39bd96d8b4599
PrasadPawar1361 <prasadpawarpp1361@gmail.com>
2026-04-23 11:18:50 +0530

    add 11th pattern

--- pattern11.java
+++ pattern11.java
@@ -6,7 +6,7 @@ public class pattern11 {
         for(int i = 1; i<=n; i++){
             for(int j=1; j<=n; j++){
 
-                if((i == j) || (i+j == 6) ){
+                if((i == j) || (i+j == n+1) ){
                 System.out.print("*  ");
                 }
                 else{
