f8fac1107699d3293e160ba0f64f5db6e2d7f666
PrasadPawar1361 <prasadpawarpp1361@gmail.com>
2026-04-26 09:36:41 +0530

    here i am make w for pawar

---
+++ patternW.java
@@ -0,0 +1,33 @@
+public class patternW {
+     public static void main(String[] args){
+
+        int height =6;
+
+        for(int i = 0; i<=height; i++){
+            
+            if(i == 3){
+                System.out.println("*     *     *");
+            }
+             
+            else if(i == 4){
+               
+                System.out.println("*   *   *   *"); 
+            }
+            else if(i == 5){
+               
+                System.out.println("*  *     *  *"); 
+            }
+            else if(i == 6){
+               
+                System.out.println("* *       * *"); 
+            }
+            
+            else{
+                               
+                System.out.println("*           *"); 
+            }
+            }
+
+                     
+        }
+    }
